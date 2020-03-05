be57443fdfda0c9ce61fb02262a820a33f757e9c
Cristiano <cristiano@blanko.be>
2020-03-05 13:30:33 -0300

    file handle functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MaterialApp(home: Home()));
 
@@ -8,6 +11,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/tasksData.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (err) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
